cbc62ccad0c07b5b0451e02267236b7a5e7b808f
Bruno Macedo <contato@brunomacedo.com.br>
2018-07-16 18:09:24 -0300

    add new tests

--- cypress/integration/tc_spec.js
+++ cypress/integration/tc_spec.js
@@ -1,16 +1,43 @@
-describe('Open Dieta - Site', () => {
-  describe('Title', () => {
-    it('should assert that title is correct', () => {
-      cy.visit('https://dieta.uol.com.br/')
+beforeEach(() => {
+  cy.visit('https://dieta.uol.com.br/')
+});
 
+describe('Open website', () => {
+  describe('Validate elements', () => {
+    it('should assert that title is correct', () => {
       cy.get('.uol-dieta #topo h1')
         .contains('Comece a perder peso com o UOL Dieta')
     });
   });
 
-  describe('Avaliação de peso - Click', () => {
-    it('should assert that the item Resultado exist on form', () => {
-      cy.contains('Ver resultado').click()
+  describe('Validate form', () => {
+    it('should assert that text "Ver Resultado" exist on form', () => {
+      cy.get('.form-avaliacao .btn[type="submit"]')
+        .contains('Ver resultado')
     })
+
+    it('should click to send form and return', () => {
+      cy.get('[type="submit"]').click()
+    });
+
+    it('should submit form', () => {
+      cy.log('filling out "peso"')
+      cy.get('[name="peso"]').type('90')
+
+      cy.log('filling out "altura"')
+      cy.get('[name="altura"]').type('1,90')
+
+      cy.log('filling out "sexo"')
+      cy.get('[name="sexo"]').select('Feminino')
+
+      cy.log('filling out "nome"')
+      cy.get('[name="nome"]').type('Jessika')
+
+      cy.log('filling out "nome"')
+      cy.get('[name="email"]').type('jessika@uol.com.br')
+
+      cy.log('submitting form')
+      cy.get('form[action="resultado.html"]').submit()
+    });
   });
 });
